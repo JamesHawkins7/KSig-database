40ecd9ae8c2ef8353e4684408b094e15b1a0228d
JamesHawkins7 <jamesbhawkins04@gmail.com>
2025-07-16 00:15:47 -0500

    Initial Commit

---
+++ KSig_Grade_Tracker/Managers/Start.cs
@@ -0,0 +1,15 @@
+using System;
+    
+namespace KSig_Grade_Tracker.Managers;
+
+public class Start
+{
+    public static void ConsoleStart()
+    {
+        Console.WriteLine("_  ______ ___ ____    ____ ____       _    ____  _____ ____  ");
+        Console.WriteLine("| |/ / ___|_ _/ ___|  / ___|  _ \\     / \\  |  _ \\| ____/ ___| ");
+        Console.WriteLine("| ' /\\___ \\| | |  _  | |  _| |_)  |  / _ \\ | | | |  _| \\___ \\ ");
+        Console.WriteLine("| . \\ ___) | | |_| | | |_| |  _ <   / ___ \\| |_| | |___ ___) |");
+        Console.WriteLine("|_|\\_\\____/___\\____|  \\____|_|  \\_\\/_/   \\_\\____/|_____|____/ ");
+    }
+}
